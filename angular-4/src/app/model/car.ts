257041a25780989888a4a613cc8f3116e4127e8b
Zolival <zolinak@gmail.com>
2022-02-28 16:59:33 +0100

    partly 3

--- angular-4/src/app/model/car.ts
+++ angular-4/src/app/model/car.ts
@@ -1,2 +1,10 @@
 export class Car {
+  [k:string]: any;
+  id: number = 1;
+  model: string = "Sierra 1500";
+  make: string = "GMC";
+  year: number = 2011;
+  price: number = 33014;
+  stock: number = 8;
+  active: boolean = true;
 }
